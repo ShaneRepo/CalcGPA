6c35522f076aefd6b9e52e8ab18220242164854f
Shane <shane8641@live.com>
2015-07-24 17:18:54 -0400

    First Commit

---
+++ MainForm.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.radFemale = New System.Windows.Forms.RadioButton()
+        Me.radMale = New System.Windows.Forms.RadioButton()
+        Me.lstGPA = New System.Windows.Forms.ListBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblAllOut = New System.Windows.Forms.Label()
+        Me.lblMaleOut = New System.Windows.Forms.Label()
+        Me.lblFemaleOut = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'radFemale
+        '
+        Me.radFemale.AutoSize = True
+        Me.radFemale.Location = New System.Drawing.Point(53, 48)
+        Me.radFemale.Name = "radFemale"
+        Me.radFemale.Size = New System.Drawing.Size(59, 17)
+        Me.radFemale.TabIndex = 1
+        Me.radFemale.TabStop = True
+        Me.radFemale.Text = "Female"
+        Me.radFemale.UseVisualStyleBackColor = True
+        '
+        'radMale
+        '
+        Me.radMale.AutoSize = True
+        Me.radMale.Location = New System.Drawing.Point(53, 93)
+        Me.radMale.Name = "radMale"
+        Me.radMale.Size = New System.Drawing.Size(48, 17)
+        Me.radMale.TabIndex = 2
+        Me.radMale.TabStop = True
+        Me.radMale.Text = "Male"
+        Me.radMale.UseVisualStyleBackColor = True
+        '
+        'lstGPA
+        '
+        Me.lstGPA.FormattingEnabled = True
+        Me.lstGPA.Location = New System.Drawing.Point(168, 48)
+        Me.lstGPA.Name = "lstGPA"
+        Me.lstGPA.Size = New System.Drawing.Size(120, 95)
+        Me.lstGPA.TabIndex = 0
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(345, 48)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.TabIndex = 3
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(345, 93)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(168, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "GPA:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(53, 208)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Average GPA"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(76, 240)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "All Students:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(76, 326)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(78, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Male Students:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(76, 405)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(89, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Female Students:"
+        '
+        'lblAllOut
+        '
+        Me.lblAllOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAllOut.Location = New System.Drawing.Point(79, 268)
+        Me.lblAllOut.Name = "lblAllOut"
+        Me.lblAllOut.Size = New System.Drawing.Size(121, 43)
+        Me.lblAllOut.TabIndex = 8
+        '
+        'lblMaleOut
+        '
+        Me.lblMaleOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblMaleOut.Location = New System.Drawing.Point(79, 352)
+        Me.lblMaleOut.Name = "lblMaleOut"
+        Me.lblMaleOut.Size = New System.Drawing.Size(121, 43)
+        Me.lblMaleOut.TabIndex = 10
+        '
+        'lblFemaleOut
+        '
+        Me.lblFemaleOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFemaleOut.Location = New System.Drawing.Point(79, 429)
+        Me.lblFemaleOut.Name = "lblFemaleOut"
+        Me.lblFemaleOut.Size = New System.Drawing.Size(121, 43)
+        Me.lblFemaleOut.TabIndex = 12
+        '
+        'MainForm
+        '
+        Me.AcceptButton = Me.btnCalc
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(476, 523)
+        Me.Controls.Add(Me.lblFemaleOut)
+        Me.Controls.Add(Me.lblMaleOut)
+        Me.Controls.Add(Me.lblAllOut)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.lstGPA)
+        Me.Controls.Add(Me.radMale)
+        Me.Controls.Add(Me.radFemale)
+        Me.Name = "MainForm"
+        Me.Text = "GPA Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents radFemale As System.Windows.Forms.RadioButton
+    Friend WithEvents radMale As System.Windows.Forms.RadioButton
+    Friend WithEvents lstGPA As System.Windows.Forms.ListBox
+    Friend WithEvents btnCalc As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents lblAllOut As System.Windows.Forms.Label
+    Friend WithEvents lblMaleOut As System.Windows.Forms.Label
+    Friend WithEvents lblFemaleOut As System.Windows.Forms.Label
+
+End Class
---
+++ MainForm.vb
@@ -0,0 +1,68 @@
+﻿Option Strict On
+Public Class MainForm
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' No default value , at least thats what I'm aiming for when form loads. ( PROBLEM SOLVED ) I had to make sure tab order did not have radio buttons = 0 'zero'
+        radFemale.Checked = False
+        radMale.Checked = False
+        ' Fill list box with values on form load
+        For decGPA As Decimal = 1D To 4D Step 0.1D
+            lstGPA.Items.Add(decGPA.ToString("N2"))
+        Next decGPA
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Closing the application
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' Calculate the ave GPA for all students
+        Static decGpaAll As Decimal
+        Static decGpaMale As Decimal
+        Static decGpaFemale As Decimal
+        Static decGpaTotal As Decimal
+        Static decGpaTotalMale As Decimal
+        Static decGpaTotalFemale As Decimal
+        Static intCountAll As Integer
+        Static intCountMale As Integer
+        Static intCountFemale As Integer
+        Static realGPA As Decimal
+        Decimal.TryParse(lstGPA.SelectedItem.ToString, decGpaAll)
+        Decimal.TryParse(lstGPA.SelectedItem.ToString, decGpaMale)
+        Decimal.TryParse(lstGPA.SelectedItem.ToString, decGpaFemale)
+
+      
+        ' Gpa calc for all students
+        decGpaAll = CDec(lstGPA.SelectedItem)
+        decGpaTotal = decGpaTotal + decGpaAll
+        intCountAll = intCountAll + 1
+        lblAllOut.Text = CStr(decGpaTotal / intCountAll)
+        ' Gpa for females
+        If radFemale.Checked = True Then
+            decGpaFemale = CDec(lstGPA.SelectedItem)
+            decGpaTotalFemale = decGpaTotalFemale + decGpaFemale
+            intCountFemale = intCountFemale + 1
+            realGPA = CDec(CStr(decGpaTotalFemale / intCountFemale))
+            lblFemaleOut.Text = CStr(realGPA)
+            ' Gpa for males
+        ElseIf radMale.Checked = True Then
+            decGpaMale = CDec(lstGPA.SelectedItem)
+            decGpaTotalMale = decGpaTotalMale + decGpaMale
+            intCountMale = intCountMale + 1
+            lblMaleOut.Text = CStr(decGpaTotalMale / intCountMale)
+        End If
+
+
+    End Sub
+
+    Private Sub lstGPA_SelectedValueChanged(sender As Object, e As EventArgs) Handles lstGPA.SelectedValueChanged
+        ' Clear the output
+        'lblAllOut.Text = String.Empty
+        'lblFemaleOut.Text = String.Empty
+        'lblMaleOut.Text = String.Empty
+    End Sub
+End Class
